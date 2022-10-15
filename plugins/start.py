2ccd1f6acafc2e90c84aaf11a939b36a76dc43d0
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-10-15 14:46:36 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,73 +2,40 @@
 # -*- coding: utf-8 -*-
 # @trojanzhex
 
-
 from pyrogram import filters
 from pyrogram import Client as trojanz
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import Config
 from script import Script
-from pyromod import message
-
-@trojanz.on_message(filters.command(["start"]) & filters.private)
-async def start(client, message):
-    await message.reply_text(
-        text=Script.START_MSG.format(message.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("HELP", callback_data="help_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
-                ]
-            ]
-        ),
-        reply_to_message_id=message.message_id
-    )
 
 
-@trojanz.on_message(filters.command(["help"]) & filters.private)
-async def help(client, message):
-    await message.reply_text(
-        text=Script.HELP_MSG,
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("BACK", callback_data="start_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
-                ]
-            ]
-        ),
-        reply_to_message_id=message.message_id
-    )
+@trojanz.on_message(filters.private & (filters.document | filters.video))
+async def confirm_dwnld(client, message):
+    media = message
+    filetype = media.document or media.video
+
+    if filetype.mime_type.startswith("video/"):
+        await message.reply_text(
+            "**What you want me to do??**",
+            quote=True,
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(text="DOWNLOAD and PROCESS",
+                                     callback_data="download_file")
+            ], [InlineKeyboardButton(text="CANCEL", callback_data="close")]]))
+    else:
+        await message.reply_text("Invalid Media", quote=True)
 
 
-@trojanz.on_message(filters.command(["about"]) & filters.private)
-async def about(client, message):
-    await message.reply_text(
-        text=Script.ABOUT_MSG,
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("BACK", callback_data="help_data"),
-                    InlineKeyboardButton("START", callback_data="start_data"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Streams-Extractor")
-                ]
-            ]
-        ),
-        reply_to_message_id=message.message_id
-    )
+@trojanz.on_message(filters.private & filters.incoming & filters.text &
+                    (filters.regex('^(ht|f)tp*')))
+async def link_dwnld(client, message):
+    if not message.media:
+        #link = message.text
+        await message.reply_text(
+            "**What you want me to do??**",
+            quote=True,
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(text="DOWNLOAD From Link",
+                                     callback_data="download_url")
+            ], [InlineKeyboardButton(text="CANCEL", callback_data="close")]]))
